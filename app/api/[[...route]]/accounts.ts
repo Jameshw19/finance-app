f910dd995c067a4b3d0f48bc88b47fe3ba239d8a
James <jameshowie17@outlook.com>
2024-05-24 19:07:39 +0100

    updated accounts.ts and schema.ts to prepare for the accounts POST API. TODO:Install paralleldrive/cuid2

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,25 +1,48 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
+import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
 
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorized" }, 401);
-  }
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
 
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId));
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-  return c.json({ data });
-});
+    return c.json({ data });
+  })
+  .post(
+    "/ ",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db.insert(accounts).values({
+        id: "test",
+        userId: auth.userId,
+        ...values,
+      });
+
+      return c.json({});
+    }
+  );
 
 export default app;
