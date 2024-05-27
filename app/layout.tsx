a8ce629fae5e58c3df77098f3f444cee5717541e
James <jameshowie17@outlook.com>
2024-05-27 12:57:51 +0100

    Added sheet-provider and new-account-sheet.tsx. SheetProvider is impoted into app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <SheetProvider />
           <QueryProvider>{children}</QueryProvider>
         </body>
       </html>
