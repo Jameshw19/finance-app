405fcc403ab858dac2ef3f1e6291b8c76bae5e5e
James <jameshowie17@outlook.com>
2024-05-28 18:54:23 +0100

    Updated accounts/page.tsx and accounts/column.tsx -Page.tsx has the useGetAccounts hook to prepare for Account deletion. -Columns.tsx Removed the type Payment and replaced it with ResponseType.Removed two accessorKey and header;Status and Amount. Updated the email accessor key  to take in ame instead.

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,34 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
-import { Payment, columns } from "./columns";
-import { DataTable } from "@/components/data-table";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "a@example.com",
-  },
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "success",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "failed",
-    email: "q@example.com",
-  },
-  // ...
-];
+import { Plus } from "lucide-react";
+import { DataTable } from "@/components/data-table";
+import { columns } from "./columns";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -46,7 +28,7 @@ const AccountsPage = () => {
           <DataTable
             filterKey="email"
             columns={columns}
-            data={data}
+            data={accounts}
             onDelete={() => {}}
             disabled={false}
           />
