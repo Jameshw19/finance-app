dd6c862eb34efae667d3574c0f2ab02db9f72b53
James <jameshowie17@outlook.com>
2024-06-24 13:11:25 +0100

    Create formatDateRange in lib/utils.ts aswell as data-grid.tsx in components that takes in the formatDateRange function and import the DataGrid component into dashboard/page.tsx -Created function formatDateRange in utils.ts that takes format and subDays from date-fns. -Used the formatDateRange util in data-grid.ts to display the last 30 days. -Imported data-grid.tsx into /(dashboard)/page.tsx.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { DataGrid } from "@/components/data-grid";
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
-export default function Home() {
+export default function DashboardPage() {
   const { onOpen } = useNewAccount();
 
   return (
-    <div>
-      <Button onClick={onOpen}>Add an Account</Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
